54169ae27286babcdeb721a46edecf4f2791abbe
Maxim <fomaxi@gmail.com>
2020-09-02 14:04:15 +0300

    implement vue integration

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,26 @@
-require('./bootstrap');
+import './bootstrap';
+
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+import App from './components/App.vue';
+import Index from './components/Index.vue';
+
+const routes = [
+    {
+        path: '/',
+        component: Index,
+    },
+];
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({ routes });
+
+const app = new Vue({
+    template: '<App></App>',
+    router,
+    components: {
+        App,
+    },
+}).$mount('#app');
---
+++ resources/js/components/Index.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>Vue Index</h1>
+</template>
