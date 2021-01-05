3b177087ac7479eed3bd67ab31f9c2e744d40edc
Maxim <fomaxi@gmail.com>
2021-01-05 17:45:53 +0300

    install sanctum

--- app/User.php
+++ app/User.php
@@ -5,9 +5,11 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
+    use HasApiTokens;
     use Notifiable;
 
     /**
