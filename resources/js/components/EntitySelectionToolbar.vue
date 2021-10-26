778c6ae55e55cd88da1b2f0dca7f956d4710c232
Maxim Soldatov <fomaxi@gmail.com>
2021-10-26 09:32:42 +0300

    fix promise callback

--- resources/js/components/EntitySelectionToolbar.vue
+++ resources/js/components/EntitySelectionToolbar.vue
@@ -76,13 +76,13 @@
 
                 const requests = items.map(item => axios.put(this.resource + '/' + item.id, {tags: item.tags}));
                 this.$emit('input', []);
-                Promise.all(requests).then(this.$emit('update'));
+                Promise.all(requests).then(() => this.$emit('update'));
             },
             deleteItems() {
                 this.$root.confirm('Delete selected items?').then(() => {
                     const requests = this.value.map(item => axios.delete(this.resource + '/' + item.id));
                     this.$emit('input', []);
-                    Promise.all(requests).then(this.$emit('update'));
+                    Promise.all(requests).then(() => this.$emit('update'));
                 });
             },
         },
