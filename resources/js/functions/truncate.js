a84b193d019585d03e8698e64ba399ebed043c20
Maxim Soldatov <fomaxi@gmail.com>
2021-12-05 14:54:21 +0300

    fix truncate

--- resources/js/functions/truncate.js
+++ resources/js/functions/truncate.js
@@ -1,5 +1,5 @@
 export default function(str = '', len = 30, clamp = '...') {
-    if (str.length > len)
+    if (str.length > len + clamp.length)
         return str.substr(0, len) + clamp;
 
     return str;
--- resources/js/functions/truncateCenter.js
+++ resources/js/functions/truncateCenter.js
@@ -1,5 +1,5 @@
 export default function(str = '', len = 30, clamp = '...') {
-    if (str.length > len)
+    if (str.length > len + clamp.length)
         return str.slice(0, Math.ceil(len / 2)) + clamp + str.slice(-Math.floor(len / 2));
 
     return str;
