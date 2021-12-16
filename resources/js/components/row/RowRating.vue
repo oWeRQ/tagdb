624831e042f6510ca700ecf564555229cb970677
Maxim Soldatov <fomaxi@gmail.com>
2021-12-16 11:34:13 +0300

    fix row rating

--- resources/js/components/row/RowRating.vue
+++ resources/js/components/row/RowRating.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="v-rating d-inline-block v-rating--readonly">
-        <button type="button" aria-label="Rating 1 of 5" class="v-icon notranslate v-icon--link theme--light mdi" :class="value >= 1 ? fullClass : emptyClass" style="font-size: 18px;"></button>
-        <button type="button" aria-label="Rating 2 of 5" class="v-icon notranslate v-icon--link theme--light mdi" :class="value >= 2 ? fullClass : emptyClass" style="font-size: 18px;"></button>
-        <button type="button" aria-label="Rating 3 of 5" class="v-icon notranslate v-icon--link theme--light mdi" :class="value >= 3 ? fullClass : emptyClass" style="font-size: 18px;"></button>
-        <button type="button" aria-label="Rating 4 of 5" class="v-icon notranslate v-icon--link theme--light mdi" :class="value >= 4 ? fullClass : emptyClass" style="font-size: 18px;"></button>
-        <button type="button" aria-label="Rating 5 of 5" class="v-icon notranslate v-icon--link theme--light mdi" :class="value >= 5 ? fullClass : emptyClass" style="font-size: 18px;"></button>
+        <button type="button" aria-label="Rating 1 of 5" class="v-icon notranslate v-icon--link theme--light mdi" :class="value == 0.5 ? halfClass : (value >= 1 ? fullClass : emptyClass)" style="font-size: 18px;"></button>
+        <button type="button" aria-label="Rating 2 of 5" class="v-icon notranslate v-icon--link theme--light mdi" :class="value == 1.5 ? halfClass : (value >= 2 ? fullClass : emptyClass)" style="font-size: 18px;"></button>
+        <button type="button" aria-label="Rating 3 of 5" class="v-icon notranslate v-icon--link theme--light mdi" :class="value == 2.5 ? halfClass : (value >= 3 ? fullClass : emptyClass)" style="font-size: 18px;"></button>
+        <button type="button" aria-label="Rating 4 of 5" class="v-icon notranslate v-icon--link theme--light mdi" :class="value == 3.5 ? halfClass : (value >= 4 ? fullClass : emptyClass)" style="font-size: 18px;"></button>
+        <button type="button" aria-label="Rating 5 of 5" class="v-icon notranslate v-icon--link theme--light mdi" :class="value == 4.5 ? halfClass : (value >= 5 ? fullClass : emptyClass)" style="font-size: 18px;"></button>
     </div>
 </template>
 
@@ -21,6 +21,9 @@ export default {
         },
         emptyClass: {
             default: 'mdi-star-outline grey--text text--lighten-1',
+        },
+        halfClass: {
+            default: 'mdi-star-half-full orange--text',
         },
         fullClass: {
             default: 'mdi-star orange--text',
