39963e23de76aeacf8b563f8fb2215e22045ef90
Maxim Soldatov <fomaxi@gmail.com>
2022-05-23 17:40:00 +0300

    fix fuzzyMatch

--- resources/js/functions/fuzzyMatch.js
+++ resources/js/functions/fuzzyMatch.js
@@ -1,7 +1,7 @@
 import memoize from './memoize';
 
 const patternRegExp = memoize(pattern => {
-    return new RegExp('^(.)' + pattern.split('').join('(.*)'), 'i');
+    return new RegExp('(.?)' + pattern.split('').join('(.*)') + '(.?)', 'i');
 });
 
 export default function(pattern, str) {
