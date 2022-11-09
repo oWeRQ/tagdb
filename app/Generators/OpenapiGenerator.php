1eff36b011a9fd01d42dfadcbfffd58988a653ee
oWeRQ <fomaxi@gmail.com>
2022-11-09 22:26:44 +0300

    update openapi generator

--- app/Generators/OpenapiGenerator.php
+++ app/Generators/OpenapiGenerator.php
@@ -2,84 +2,149 @@
 
 namespace App\Generators;
 
+use App\Preset;
 use App\Token;
 
 class OpenapiGenerator
 {
+    protected $title = '';
+    protected $paths = [];
+
     public function fromToken(Token $token)
     {
-        $paths = [];
+        $this->addOpenapiMethods();
+        $this->loadToken($token);
+        return $this->generate();
+    }
+
+    public function loadToken(Token $token)
+    {
+        $this->setTitle($token->name);
+
         foreach ($token->presets as $preset) {
-            $access = $preset->access;
-            $resource = '/'.$preset->name;
-            $resourceId = $resource.'/:id';
+            $this->addPresetMethods($preset);
+        }
+    }
 
-            if ($access->can_read) {
-                $paths[$resource]['get'] = [
-                    'responses' => [
-                        '200' => ['description' => 'List entities'],
-                        '401' => ['description' => 'Bad api key'],
-                        '403' => ['description' => 'Method not allowed'],
-                        '404' => ['description' => 'Preset not found'],
-                    ],
-                ];
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
 
-                $paths[$resourceId]['get'] = [
-                    'responses' => [
-                        '200' => ['description' => 'Single entity'],
-                        '401' => ['description' => 'Bad api key'],
-                        '403' => ['description' => 'Method not allowed'],
-                        '404' => ['description' => 'Preset or entity not found'],
-                    ],
-                ];
-            }
+    public function addMethod(string $method, string $resource, array $operation)
+    {
+        $this->paths[$resource][$method] = $operation;
+    }
 
-            if ($access->can_create) {
-                $paths[$resource]['post'] = [
-                    'responses' => [
-                        '200' => ['description' => 'Created entity'],
-                        '401' => ['description' => 'Bad api key'],
-                        '403' => ['description' => 'Method not allowed'],
-                        '404' => ['description' => 'Preset not found'],
-                    ],
-                ];
-            }
+    public function addOpenapiMethods()
+    {
+        $this->addMethod('get', '/openapi', [
+            'summary' => 'Get this document',
+            'responses' => [
+                '200' => ['description' => 'Success'],
+                '401' => ['description' => 'Bad api key'],
+            ],
+        ]);
+    }
 
-            if ($access->can_update) {
-                $paths[$resourceId]['put'] = [
-                    'responses' => [
-                        '204' => ['description' => 'Success'],
-                        '401' => ['description' => 'Bad api key'],
-                        '403' => ['description' => 'Method not allowed'],
-                        '404' => ['description' => 'Preset or entity not found'],
-                    ],
-                ];
-            }
+    public function addPresetMethods(Preset $preset)
+    {
+        $access = $preset->access;
+        $name = $preset->name;
+        $resource = '/presets/'.$preset->name;
+        $resourceId = $resource.'/{id}';
+        $parameterId = [
+            'name' => 'id',
+            'in' => 'path',
+            'description' => 'Entity ID',
+            'required' => true,
+            'schema' => [
+                'type' => 'integer',
+            ],
+        ];
 
-            if ($access->can_delete) {
-                $paths[$resourceId]['delete'] = [
-                    'responses' => [
-                        '204' => ['description' => 'Success'],
-                        '401' => ['description' => 'Bad api key'],
-                        '403' => ['description' => 'Method not allowed'],
-                        '404' => ['description' => 'Preset or entity not found'],
-                    ],
-                ];
-            }
+        if ($access->can_read) {
+            $this->addMethod('get', $resource, [
+                'summary' => "List $name",
+                'responses' => [
+                    '200' => ['description' => 'List entities'],
+                    '401' => ['description' => 'Bad api key'],
+                    '403' => ['description' => 'Method not allowed'],
+                    '404' => ['description' => 'Preset not found'],
+                ],
+            ]);
+
+            $this->addMethod('get', $resourceId, [
+                'summary' => "Single $name",
+                'parameters' => [
+                    $parameterId,
+                ],
+                'responses' => [
+                    '200' => ['description' => 'Single entity'],
+                    '401' => ['description' => 'Bad api key'],
+                    '403' => ['description' => 'Method not allowed'],
+                    '404' => ['description' => 'Preset or entity not found'],
+                ],
+            ]);
         }
 
+        if ($access->can_create) {
+            $this->addMethod('post', $resource, [
+                'summary' => "Create $name",
+                'responses' => [
+                    '200' => ['description' => 'Created entity'],
+                    '401' => ['description' => 'Bad api key'],
+                    '403' => ['description' => 'Method not allowed'],
+                    '404' => ['description' => 'Preset not found'],
+                ],
+            ]);
+        }
+
+        if ($access->can_update) {
+            $this->addMethod('put', $resourceId, [
+                'summary' => "Update $name",
+                'parameters' => [
+                    $parameterId,
+                ],
+                'responses' => [
+                    '204' => ['description' => 'Success'],
+                    '401' => ['description' => 'Bad api key'],
+                    '403' => ['description' => 'Method not allowed'],
+                    '404' => ['description' => 'Preset or entity not found'],
+                ],
+            ]);
+        }
+
+        if ($access->can_delete) {
+            $this->addMethod('delete', $resourceId, [
+                'summary' => "Delete $name",
+                'parameters' => [
+                    $parameterId,
+                ],
+                'responses' => [
+                    '204' => ['description' => 'Success'],
+                    '401' => ['description' => 'Bad api key'],
+                    '403' => ['description' => 'Method not allowed'],
+                    '404' => ['description' => 'Preset or entity not found'],
+                ],
+            ]);
+        }
+    }
+
+    public function generate()
+    {
         return [
             'openapi' => '3.0.0',
             'info' => [
-                'title' => $token->name,
+                'title' => $this->title,
                 'version' => '1.0.0'
             ],
             'servers' => [
                 [
-                    'url' => env('APP_URL').'/token/presets',
+                    'url' => env('APP_URL').'/token',
                 ],
             ],
-            'paths' => array_filter($paths),
+            'paths' => array_filter($this->paths),
             'components' => [
                 'securitySchemes' => [
                     'bearerAuth' => [
